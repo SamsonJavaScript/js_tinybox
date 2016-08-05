bfe4708152b3d5755009532930687c2c7b736eda
getmanenko <sasha.getmanenko@gmail.com>
2016-08-05 17:11:31 +0300

    Update samson.php

--- samson.php
+++ samson.php
@@ -1,19 +1,14 @@
 <?php
-namespace samson\js\tinybox;
+namespace samsonjavascript\tinybox;
 
 /**
- * Интерфейс для подключения модуля в ядро фреймворка SamsonPHP
+ * Tinybox module connector
  *
- * @package SamsonPHP
  * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
  * @author Nikita Kotenko <nick.w2r@gmail.com>
- * @version 0.1
  */
 class TinyBox extends \samson\core\CompressableExternalModule
 {	
 	/** Identifier */
-	protected $id = 'tinybox';	
-	
-	/** Module dependencies */
-	protected $requirements = array('samsonjs');
+	protected $id = 'tinybox';
 }
